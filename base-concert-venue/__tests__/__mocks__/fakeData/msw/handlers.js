f6e19428ed09bc61368692fcc9c82b86fa5fe83a
Cristian Franco <cfrancobedoya@gmail.com>
2023-01-22 18:12:08 -0500

    MASTER: Add dynamic responses and get querys from Mock Service

--- base-concert-venue/__tests__/__mocks__/fakeData/msw/handlers.js
+++ base-concert-venue/__tests__/__mocks__/fakeData/msw/handlers.js
@@ -5,10 +5,20 @@ import { rest } from "msw";
 export const handlers = [
   rest.get("http://localhost:3000/api/shows/:showId", async (req, res, ctx) => {
     const { fakeShows } = await readFakeData();
-    return res(ctx.json({ show: fakeShows[0] }));
+    const { showId } = req.params;
+
+    // index / showId = 0 hast seats available in fake data
+    // index /showId = 1 has NO seats available
+    return res(ctx.json({ show: fakeShows[Number(showId)] }));
   }),
   rest.get(
     "http://localhost:3000/api/users/:userId/reservations",
-    (req, res, ctx) => res(ctx.json({ userReservations: fakeUserReservations }))
+    (req, res, ctx) => {
+      const { userId } = req.params;
+
+      // return fakeUserReservations if userId is 1; empty array otherwise
+      const userReservations = Number(userId) === 1 ? fakeUserReservations : [];
+      return res(ctx.json({ userReservations }));
+    }
   ),
 ];
